ef17dab3229b5592db3782d8fdc61e8249477490
morgankn <118085617+morgankn@users.noreply.github.com>
2023-01-07 11:26:28 -0500

    added html

---
+++ script.js
@@ -0,0 +1,6 @@
+//When btn is clicked run the fetch 
+//set an event listener on btn 
+var cityInputEl = document.querySelector('#city-input');
+var searchBtnEl = document.querySelector('#search-btn');
+
+
